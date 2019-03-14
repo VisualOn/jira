c37519818deb92c60afa9056b078d9398ef17b2e
Michael Kriese <michael.kriese@visualon.de>
2019-03-14 18:56:09 +0100

    test: add curl

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -7,9 +7,11 @@ docker rm -f jiratest
 $ErrorActionPreference = 'Stop';
 Write-Host Starting container
 docker run --name jiratest -p 8080:8080 -d jira
-Start-Sleep 10
+Start-Sleep 20
 
-docker exec -t jiratest java --version
+docker exec -t jiratest java -version
+
+Invoke-WebRequest http://localhost:8080
 
 docker logs jiratest
 
