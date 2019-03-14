eabd830f144554899c43975648d3f9fda1e04a63
Michael Kriese <michael.kriese@visualon.de>
2019-03-14 18:08:06 +0100

    ci: add deployment

---
+++ scripts/test.ps1
@@ -0,0 +1,16 @@
+Write-Host Starting test
+
+$ErrorActionPreference = 'SilentlyContinue';
+docker kill jiratest
+docker rm -f jiratest
+
+$ErrorActionPreference = 'Stop';
+Write-Host Starting container
+docker run --name jiratest -p 8080:8080 -d jira
+Start-Sleep 10
+
+docker logs jiratest
+
+$ErrorActionPreference = 'SilentlyContinue';
+docker kill jiratest
+docker rm -f jiratest
