a314d39f475277a741e014bf113226008fcbb33d
Michael Kriese <michael.kriese@visualon.de>
2019-03-14 19:13:50 +0100

    ci: fix init

--- scripts/init.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Stop';
-
-git config --global core.autocrlf true
-
-if ($env:APPVEYOR_REPO_TAG -eq "true")
-{
-    Update-AppveyorBuild -Version ($env:APPVEYOR_REPO_TAG_NAME).TrimStart("v")
-}
